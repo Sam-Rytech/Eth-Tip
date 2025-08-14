46e12cf005454d19cf79f33583068c4576e216dc
Omolayo Temitayo <temitayoomolayo@gmail.com>
2025-08-14 01:11:40 +0100

    History page added

---
+++ app/history/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+import TipList from '../components/TipList'
+
+export default function HistoryPage() {
+  return (
+    <div className="p-6 rounded-lg border bg-white dark:bg-gray-900">
+      <h2 className="font-semibold text-lg mb-4">Your Tip History</h2>
+      <TipList personal />
+    </div>
+  )
+}
